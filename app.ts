d75812886f04c27c369c3f3f4fbbf72d1ed05fa0
J^G2 <devcurses@gmail.com>
2024-03-13 13:01:13 -0600

    Init commit

---
+++ app.ts
@@ -0,0 +1,13 @@
+// let msg: string = 'Hello world';
+
+// const hero = {
+//     name: 'Ironman',
+//     age: 45
+// }
+
+// // hero.age = '50';
+
+// console.log(msg, hero.age);
+
+const a: number = 10;
+console.log(a);
