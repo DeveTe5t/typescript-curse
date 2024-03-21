de61b0f68c98338e5fa4b3ede85c714d77edea88
J^G2 <devcurses@gmail.com>
2024-03-21 15:16:59 -0600

    Homework types section

--- app.ts
+++ app.ts
@@ -1,6 +1,52 @@
-// self-invoking anonymous function
 (() => {
 
-    const a: number = 10;
-    console.log(a);
-})();
+  // Tipos
+  const batman: string = 'Bruce';
+  const superman: string = 'Clark';
+
+  const existe: boolean = false;
+
+  // Tuplas
+  const parejaHeroes: [string, string] = [batman, superman];
+  const villano: [string, number, boolean] = ['Lex Lutor', 5, true];
+
+  // Arreglos
+  const aliados: string[] = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+
+  //Enumeraciones
+  enum PowerHero {
+    acuaman = 0,
+    batman = 1,
+    flash = 5,
+    superman = 100
+  }
+  const fuerzaFlash: PowerHero = PowerHero.acuaman;
+  const fuerzaSuperman: PowerHero = PowerHero.superman;
+  const fuerzaBatman: PowerHero = PowerHero.batman;
+  const fuerzaAcuaman: PowerHero = PowerHero.acuaman;
+
+  // Retorno de funciones
+  function activar_batiseñal(): string {
+    return 'activada';
+  }
+
+  function pedir_ayuda(): void {
+    console.log('Auxilio!!!');
+  }
+
+  // Aserciones de Tipo
+  const poder: any = '100';
+  const largoDelPoder: number = (poder as string).length;
+  console.log(largoDelPoder);
+
+  enum enumeracion {
+    a = 10,
+    b,
+    c = 9,
+    d
+  }
+
+  console.log('cual' + enumeracion.d);
+
+})()
+
