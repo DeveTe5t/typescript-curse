e347dce5c432bc5afc7f27bc811966b6cae45f8e
DeveTe5t <devcurses@gmail.com>
2024-04-15 10:51:52 -0600

    pubic and private method

--- classes/basic.ts
+++ classes/basic.ts
@@ -16,6 +16,9 @@
 
         // Short way
         static avgAge: number = 35;
+        static getAvgAge() {
+            return this.name;
+        }
 
         constructor(
             private name: string,
@@ -25,10 +28,15 @@
         ) {
             // Avenger.avgAge = avgAge;
         }
+
+        public bio() {
+            return `${this.name} (${this.team})!!!`;
+        }
     }
 
     // const antman = new Avenger('Antman', 'Captain', 'Scott Lang', 37);
     const antman = new Avenger('Antman', 'Captain', 'Scott Lang');
     // console.log(antman, Avenger.avgAge);
     console.log(antman);
+    console.log(Avenger.getAvgAge());
 })()
